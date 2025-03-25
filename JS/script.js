f61a44cdeea32e909434e604baa369721cb20d48
maruhsilva <marua.desenvolvedor@gmail.com>
2025-03-25 12:39:03 -0300

    25/03/25

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const overlay = document.querySelector('.menu-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+hamburger.addEventListener('click', () => {
+  menu.classList.add('show');
+  overlay.classList.add('show');
+  hamburger.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  menu.classList.remove('show');
+  overlay.classList.remove('show');
+  hamburger.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
+  menu.classList.remove('show');
+  overlay.classList.remove('show');
+  hamburger.classList.remove('active');
+});
